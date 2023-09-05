2dffb9c9e11ccbfa103d90b832ddc62453d82d4d
NooshineTalebi <talebinooshine@gmail.com>
2023-09-05 09:31:43 +0330

    refactoring code

--- script.js
+++ script.js
@@ -3,17 +3,22 @@ let secretNumber = Math.trunc(Math.random()*20) + 1;
 let score = '20';
 let highscore = 0;
 
+const displayMessage = function (message) {
+    document.querySelector('.message').textContent = message;
+}
 
     const buttonCheck = document.querySelector('.button-check').addEventListener('click',function () {
     const guess = Number(document.querySelector('.guess').value);
 
     //when there is no input
     if(!guess){
-        document.querySelector('.message').textContent = '⛔ No Number';
+        // document.querySelector('.message').textContent = '⛔ No Number';
+        displayMessage('⛔ No Number');
     //when player wins
     }else if(guess === secretNumber){
         document.querySelector('.number').textContent = secretNumber;
-        document.querySelector('.message').textContent = '🎉 Correct Number';
+        // document.querySelector('.message').textContent = '🎉 Correct Number';
+        displayMessage('🎉 Correct Number');
         document.querySelector('body').style.backgroundColor = '#D2DE32';
         document.querySelector('.guess').style.backgroundColor = '#D2DE32';
         document.querySelector('.number').style.width = '12rem';
@@ -22,32 +27,49 @@ let highscore = 0;
             highscore = score;
             document.querySelector('.highscore').textContent = highscore;
         }
-    //when guess is too high
-    }else if(guess > secretNumber){
-        if(score > 1){
-            document.querySelector('.message').textContent = '📈 Too High';
-            score--;
-            document.querySelector('.score').textContent = score;
-        }else{
-            document.querySelector('.message').textContent = '💥 You Lost The Game';
-            document.querySelector('body').style.backgroundColor = 'red';
-            document.querySelector('.guess').style.backgroundColor = 'red';
-            document.querySelector('.score').textContent = 0;
-        }
 
-        //when guess is too low
-    }else if(guess < secretNumber){
-        if(score > 1){
-            document.querySelector('.message').textContent = '📈 Too Low';
+    //when guess is wrong
+    }else if (guess != secretNumber) {
+        if (score > 1) {
+            // document.querySelector('.message').textContent = guess > secretNumber ? '📈 Too High' : '📈 Too Low';
+            displayMessage(guess > secretNumber ? '📈 Too High' : '📈 Too Low');
             score--;
             document.querySelector('.score').textContent = score;
         }else{
-            document.querySelector('.message').textContent = '💥 You Lost The Game';
-            document.querySelector('body').style.backgroundColor = 'red';
-            document.querySelector('.guess').style.backgroundColor = 'red';
-            document.querySelector('.score').textContent = 0;
+        // document.querySelector('.message').textContent = '💥 You Lost The Game';
+        displayMessage('💥 You Lost The Game');
+        document.querySelector('body').style.backgroundColor = 'red';
+        document.querySelector('.guess').style.backgroundColor = 'red';
+        document.querySelector('.score').textContent = 0;
         }
     }
+    
+    //when guess is too high
+    // else if(guess > secretNumber){
+    //     if(score > 1){
+    //         document.querySelector('.message').textContent = '📈 Too High';
+    //         score--;
+    //         document.querySelector('.score').textContent = score;
+    //     }else{
+    //         document.querySelector('.message').textContent = '💥 You Lost The Game';
+    //         document.querySelector('body').style.backgroundColor = 'red';
+    //         document.querySelector('.guess').style.backgroundColor = 'red';
+    //         document.querySelector('.score').textContent = 0;
+    //     }
+
+    //     //when guess is too low
+    // }else if(guess < secretNumber){
+    //     if(score > 1){
+    //         document.querySelector('.message').textContent = '📈 Too Low';
+    //         score--;
+    //         document.querySelector('.score').textContent = score;
+    //     }else{
+    //         document.querySelector('.message').textContent = '💥 You Lost The Game';
+    //         document.querySelector('body').style.backgroundColor = 'red';
+    //         document.querySelector('.guess').style.backgroundColor = 'red';
+    //         document.querySelector('.score').textContent = 0;
+    //     }
+    // }
 })
 
     document.querySelector('.btn-again').addEventListener('click' , function() {
